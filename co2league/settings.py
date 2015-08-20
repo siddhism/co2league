5a86543cf79a553ffab2abcbcfce94e2299940fc
siddhesh suthar <siddhesh.it@gmail.com>
2015-08-20 17:03:03 +0530

    Added static files

--- co2league/settings.py
+++ co2league/settings.py
@@ -101,3 +101,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Template location
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "static", "templates"),
+)
+
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static", "static-only")
+    MEDIA_ROOT =  os.path.join(BASE_DIR, "static", "media")
+    STATICFILES_DIRS =    (
+        os.path.join(BASE_DIR, "static", "static"),
+    )
